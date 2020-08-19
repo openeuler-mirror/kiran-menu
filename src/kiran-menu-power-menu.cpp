b4ee632d5c5e084bf7311e97f54043b64eb5a449
tangjie02 <tangjie02@kylinsec.com.cn>
2020-08-19 13:38:05 +0800

    refactor[menu,power]: Use dynamic allocation for items in power menu
    - 对开始菜单的电源菜单中的菜单项实行动态分配
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/kiran-menu-power-menu.cpp
+++ src/kiran-menu-power-menu.cpp
@@ -3,49 +3,55 @@
 #include <glibmm/i18n.h>
 #include <iostream>
 #include "kiranhelper.h"
+#include "global.h"
 
 KiranMenuPowerMenu::KiranMenuPowerMenu()
 {
-    logout_item.set_label(_("Logout"));
-    suspend_item.set_label(_("Suspend"));
-    hibernate_item.set_label(_("Hibernate"));
-    shutdown_item.set_label(_("Shutdown"));
-    reboot_item.set_label(_("Reboot"));
-    switchuser_item.set_label(_("Switch user"));
+    switchuser_item = Gtk::make_managed<Gtk::MenuItem>(_("Switch user"));
+    switchuser_item->signal_activate().connect(sigc::hide_return(
+        sigc::ptr_fun(&KiranPower::switch_user)));
 
-    append(switchuser_item);
-    append(logout_item);
-    if (KiranPower::can_suspend())
-        append(suspend_item);
+    append(*switchuser_item);
 
-    if (KiranPower::can_hibernate())
-        append(hibernate_item);
-    append(reboot_item);
-    append(shutdown_item);
-
-
-
-    logout_item.signal_activate().connect(sigc::hide_return(
+    logout_item = Gtk::make_managed<Gtk::MenuItem>(_("Logout"));
+    logout_item->signal_activate().connect(sigc::hide_return(
         sigc::bind<int>(sigc::ptr_fun(&KiranPower::logout), LOGOUT_MODE_NOW)));
-    suspend_item.signal_activate().connect(sigc::hide_return(sigc::ptr_fun(&KiranPower::suspend)));
-    hibernate_item.signal_activate().connect(sigc::hide_return(sigc::ptr_fun(&KiranPower::hibernate)));
-    reboot_item.signal_activate().connect(sigc::hide_return(sigc::ptr_fun(&KiranPower::reboot)));
-    shutdown_item.signal_activate().connect(sigc::hide_return(sigc::ptr_fun(&KiranPower::shutdown)));
-    switchuser_item.signal_activate().connect(sigc::hide_return(sigc::ptr_fun(&KiranPower::switch_user)));
+    append(*logout_item);
 
-    logout_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
-    suspend_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
-    hibernate_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
-    reboot_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
-    shutdown_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
-    switchuser_item.signal_activate().connect(sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
+    if (KiranPower::can_suspend()) {
+        suspend_item = Gtk::make_managed<Gtk::MenuItem>(_("Suspend"));
+        suspend_item->signal_activate().connect(sigc::hide_return(
+            sigc::ptr_fun(&KiranPower::suspend)));
+
+        append(*suspend_item);
+    }
+
+    if (KiranPower::can_hibernate()) {
+        hibernate_item = Gtk::make_managed<Gtk::MenuItem>(_("Hibernate"));
+        hibernate_item->signal_activate().connect(sigc::hide_return(
+            sigc::ptr_fun(&KiranPower::hibernate)));
+
+        append(*hibernate_item);
+    }
+
+    reboot_item = Gtk::make_managed<Gtk::MenuItem>(_("Reboot"));
+    reboot_item->signal_activate().connect(
+        sigc::hide_return(sigc::ptr_fun(&KiranPower::reboot)));
+    append(*reboot_item);
+
+    shutdown_item = Gtk::make_managed<Gtk::MenuItem>(_("Shutdown"));
+    shutdown_item->signal_activate().connect(
+        sigc::hide_return(sigc::ptr_fun(&KiranPower::shutdown)));
+    append(*shutdown_item);
+
+    for (auto child: get_children()) {
+        Gtk::MenuItem *item = dynamic_cast<Gtk::MenuItem*>(child);
+        //点击电源菜单选项后收起开始菜单窗口
+        item->signal_activate().connect(
+            sigc::mem_fun(*this, &KiranMenuPowerMenu::hide_menu_window));
+    }
 
     get_style_context()->add_class("power-menu");
-}
-
-KiranMenuPowerMenu::~KiranMenuPowerMenu()
-{
-
 }
 
 void KiranMenuPowerMenu::hide_menu_window()
@@ -56,24 +62,4 @@ void KiranMenuPowerMenu::hide_menu_window()
         return;
     menu_window = this->get_attach_widget()->get_toplevel();
     menu_window->hide();
-}
-
-bool KiranMenuPowerMenu::on_map_event(GdkEventAny *any_event)
-{
-    bool res;
-
-    res = Gtk::Menu::on_map_event(any_event);
-
-        select_item(logout_item);;
-    return res;
-}
-
-bool KiranMenuPowerMenu::on_focus_in_event(GdkEventFocus *focus_event)
-{
-    bool res;
-
-    res = Gtk::Menu::on_focus_in_event(focus_event);
-        logout_item.grab_focus();
-    std::cout<<"focus in"<<std::endl;
-    return res;
 }
--- src/kiran-menu-power-menu.h
+++ src/kiran-menu-power-menu.h
@@ -8,21 +8,17 @@ class KiranMenuPowerMenu : public KiranOpacityMenu
 {
 public:
     KiranMenuPowerMenu();
-    ~KiranMenuPowerMenu();
+    ~KiranMenuPowerMenu() = default;
 
     void hide_menu_window();
 
-protected:
-    virtual bool on_map_event(GdkEventAny *any_event) override;
-    virtual bool on_focus_in_event(GdkEventFocus *focus_event) override;
-
 private:
-    Gtk::MenuItem suspend_item;
-    Gtk::MenuItem hibernate_item;
-    Gtk::MenuItem shutdown_item;
-    Gtk::MenuItem reboot_item;
-    Gtk::MenuItem logout_item;
-    Gtk::MenuItem switchuser_item;
+    Gtk::MenuItem *suspend_item;
+    Gtk::MenuItem *hibernate_item;
+    Gtk::MenuItem *shutdown_item;
+    Gtk::MenuItem *reboot_item;
+    Gtk::MenuItem *logout_item;
+    Gtk::MenuItem *switchuser_item;
 };
 
 #endif // KIRANMENUPOWERMENU_H
