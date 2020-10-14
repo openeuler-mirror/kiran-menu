b33c48d0bbc1488c40cd28fe998c4933fdec5c62
tangjie02 <tangjie02@kylinsec.com.cn>
2020-10-14 09:38:48 +0800

    refactor[tasklist]: Adjust processing logic for thumbnail context menu items
    - 调整优化窗口缩略图右键菜单项的处理逻辑
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/tasklist-window-context-menu.cpp
+++ src/tasklist-window-context-menu.cpp
@@ -20,18 +20,17 @@ void TasklistWindowContextMenu::refresh()
     Gtk::RadioButtonGroup group;
 
     KiranHelper::remove_all_for_container(*this);
-    auto window = this->win.lock();
+    auto window = win.lock();
 
     if (!window)
         return;
 
-    item = Gtk::manage(new Gtk::MenuItem(_("Maximize")));
-    
+    /* 最大化 */
+    item = Gtk::make_managed<Gtk::MenuItem>(_("Maximize"));
     item->signal_activate().connect(
                 [this]() -> void {
-                    if (!this->win.expired()) {
-
-                        auto window = this->win.lock();
+                    auto window = win.lock();
+                    if (window) {
                         if (window->is_minimized()) {
                             //如果窗口已经最小化，需要先恢复窗口大小
                             window->unminimize(gtk_get_current_event_time());
@@ -41,85 +40,88 @@ void TasklistWindowContextMenu::refresh()
                 });
     append(*item);
 
-    item = Gtk::manage(new Gtk::MenuItem(_("Minimize")));
+    /* 最小化 */
+    item = Gtk::make_managed<Gtk::MenuItem>(_("Minimize"));
     item->signal_activate().connect(
                 [this]() -> void {
-                    if (!this->win.expired())
-                        this->win.lock()->minimize();
+                    auto window = win.lock();
+                    if (window)
+                        window->minimize();
                 });
     append(*item);
 
-    item = Gtk::manage(new Gtk::MenuItem(_("Restore")));
+    /* 恢复大小 */
+    item = Gtk::make_managed<Gtk::MenuItem>(_("Restore"));
     item->signal_activate().connect(
                 [this]() -> void {
-                    if (!this->win.expired()) {
-                        std::shared_ptr<Kiran::Window> win;
-                        win = this->win.lock();
+                    auto window = win.lock();
+                    if (window) {
                         GdkEvent *event = gtk_get_current_event();
 
-                        if (win->is_minimized())
-                            win->unminimize(gdk_event_get_time(event));
+                        if (window->is_minimized())
+                            window->unminimize(gdk_event_get_time(event));
 
-                        if (win->is_maximized())
-                            win->unmaximize();
+                        if (window->is_maximized())
+                            window->unmaximize();
                     }
 
                 });
     append(*item);
 
-    check_item = Gtk::manage(new Gtk::CheckMenuItem(_("Always on top")));
+    /* 总是置顶 */
+    check_item = Gtk::make_managed<Gtk::CheckMenuItem>(_("Always on top"));
     check_item->signal_activate().connect(
                 [this, check_item]() -> void {
-                    if (!this->win.expired()) {
-                        auto window = this->win.lock();
+                    auto window = win.lock();
+                    if (window) {
                         if (check_item->get_active())
                             window->make_above();
-                        else {
+                        else
                             window->make_unabove();
-                        }
                     }
 
                 });
-    check_item->set_active(this->win.lock()->is_above());
+    check_item->set_active(window->is_above());
     append(*check_item);
 
-    radio_item = Gtk::manage(new Gtk::RadioMenuItem(group, _("Only on this workspace")));
+    /* 仅在当前工作区 */
+    radio_item = Gtk::make_managed<Gtk::RadioMenuItem>(group, _("Only on this workspace"));
     radio_item->signal_toggled().connect(
                 [this, radio_item]() -> void {
-                    if (!this->win.expired() && radio_item->get_active()) {
-                        this->win.lock()->set_on_visible_workspace(false);
-                    }
+                    auto window = win.lock();
+                    if (window && radio_item->get_active())
+                        window->set_on_visible_workspace(false);
                 });
     append(*radio_item);
-    radio_item->set_active(true);
+    radio_item->set_active(!window->get_on_visible_workspace());
 
-    radio_item = Gtk::manage(new Gtk::RadioMenuItem(group, _("Always on visible workspace")));
+    /* 总在可见工作区 */
+    radio_item = Gtk::make_managed<Gtk::RadioMenuItem>(group, _("Always on visible workspace"));
     radio_item->signal_toggled().connect(
                 [this, radio_item]() -> void {
-                    if (!this->win.expired() && radio_item->get_active()) {
-                        this->win.lock()->set_on_visible_workspace(true);
-                    }
+                    auto window = win.lock();
+                    if (window && radio_item->get_active())
+                        window->set_on_visible_workspace(true);
                 });
     append(*radio_item);
-    if (window->get_on_visible_workspace())
-        radio_item->set_active(true);
+    radio_item->set_active(window->get_on_visible_workspace());
 
-    item = Gtk::manage(new Gtk::MenuItem(_("Move to another workspace")));
-    do {
-        //创建工作区列表子菜单
-        auto submenu = create_workspace_menu();
-
-        g_assert(submenu != nullptr);
-        item->set_submenu(*submenu);
-        submenu->show_all();
-    } while (0);
+    /* 移动到其它工作区 */
+    item = Gtk::make_managed<Gtk::MenuItem>(_("Move to another workspace"));
+    auto submenu = create_workspace_submenu();
+    item->set_submenu(*submenu);
+    submenu->show_all();
     append(*item);
 
-    item = Gtk::manage(new Gtk::MenuItem(_("Close Window")));
+    /* 关闭窗口选项 */
+    item = Gtk::make_managed<Gtk::MenuItem>(_("Close Window"));
     item->signal_activate().connect(
                 [this]() -> void {
-                    if (!this->win.expired())
-                        this->win.lock()->close();
+                    auto window = win.lock();
+                    if (!window)
+                        return;
+
+                    window->close();
                 });
     append(*item);
 
@@ -129,35 +131,41 @@ void TasklistWindowContextMenu::refresh()
 /**
  * 创建工作区列表菜单
  */
-Gtk::Menu *TasklistWindowContextMenu::create_workspace_menu(void)
+Gtk::Menu *TasklistWindowContextMenu::create_workspace_submenu(void)
 {
     Gtk::MenuItem *item;
-    KiranOpacityMenu *menu = Gtk::manage(new KiranOpacityMenu());
-    Kiran::WorkspaceManager *manager = Kiran::WorkspaceManager::get_instance();
-
-    auto window = this->win.lock();
+    auto window = win.lock();
 
     if (!window)
         return nullptr;
 
-    for (auto workspace: manager->get_workspaces()) {
-        item = Gtk::manage(new Gtk::MenuItem(workspace->get_name()));
+    auto menu = Gtk::make_managed<KiranOpacityMenu>();
+    for (auto workspace: Kiran::WorkspaceManager::get_instance()->get_workspaces()) {
+        item = Gtk::make_managed<Gtk::MenuItem>(workspace->get_name());
         item->signal_activate().connect(
-                    [workspace, this]() -> void {
-            /**
-             * 将窗口移动到对应的工作区
-             */
-            auto window = this->win.lock();
-            if (window)
-                window->move_to_workspace(workspace);
-        });
+                    sigc::bind<int>(
+                        sigc::mem_fun(*this, &TasklistWindowContextMenu::move_window_to_workspace),
+                        workspace->get_number()));
 
         menu->append(*item);
-        if (workspace == window->get_workspace())
+        if (workspace == window->get_workspace()) {
+            /* 当前工作区不可点击 */
             item->set_sensitive(false);
+        }
     }
 
     menu->get_style_context()->add_class("previewer-context-menu");
-
     return menu;
+}
+
+/**
+ * 将窗口移动到对应的工作区
+ */
+void TasklistWindowContextMenu::move_window_to_workspace(int workspace_no)
+{
+    auto window = win.lock();
+    auto workspace = Kiran::WorkspaceManager::get_instance()->get_workspace(workspace_no);
+
+    if (window && workspace)
+        window->move_to_workspace(workspace);
 }
--- src/tasklist-window-context-menu.h
+++ src/tasklist-window-context-menu.h
@@ -8,12 +8,26 @@ class TasklistWindowContextMenu : public KiranOpacityMenu
 {
 public:
     TasklistWindowContextMenu(const std::shared_ptr<Kiran::Window> &win_);
+
+    /**
+     * @brief refresh 根据关联的窗口状态刷新菜单内容
+     */
     void refresh();
 protected:
-    Gtk::Menu *create_workspace_menu(void);
+    /**
+     * @brief create_workspace_submenu 创建工作区列表子菜单，供工作区切换使用
+     * @return 创建的子菜单
+     */
+    Gtk::Menu *create_workspace_submenu(void);
+
+    /**
+     * @brief move_window_to_workspace 将关联的窗口移动到指定的工作区
+     * @param workspace_no             目标工作区的下标
+     */
+    void move_window_to_workspace(int workspace_no);
 
 private:
-    std::weak_ptr<Kiran::Window> win;
+    std::weak_ptr<Kiran::Window> win;   /* 该菜单关联的窗口 */
 };
 
 #endif // TASKLIST_WINDOW_CONTEXT_MENU_H
