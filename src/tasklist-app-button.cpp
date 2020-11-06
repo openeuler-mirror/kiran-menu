37b3a183dd2661285b04491ecc0e414abea2f40b
tangjie02 <tangjie02@kylinsec.com.cn>
2020-11-06 13:34:16 +0800

    fix[taskbar]: Use first window icon for app buttons that has no icon
    - 对于没有图标的应用，在任务栏上的应用按钮中显示该应用第一个已打开窗口的窗口图标
    
    Signed-off-by: tangjie02 <tangjie02@kylinsec.com.cn>

--- src/tasklist-app-button.cpp
+++ src/tasklist-app-button.cpp
@@ -217,7 +217,7 @@ bool TasklistAppButton::on_draw(const::Cairo::RefPtr<Cairo::Context> &cr)
 
             if (icon_desc[0] == '/') {
                 pixbuf = Gdk::Pixbuf::create_from_file(icon_desc, icon_size * scale, icon_size * scale);
-	    }
+            }
             else
             {
                 auto icon_theme = Gtk::IconTheme::get_default();
@@ -226,6 +226,12 @@ bool TasklistAppButton::on_draw(const::Cairo::RefPtr<Cairo::Context> &cr)
                                                scale,
                                                Gtk::ICON_LOOKUP_FORCE_SIZE);
             }
+        } else {
+            /*
+             * 无法获取到应用图标的情况下，使用应用第一个已打开窗口的图标作为应用图标
+             */
+            pixbuf = Glib::wrap(app_->get_taskbar_windows().front()->get_icon(), true);
+            pixbuf = pixbuf->scale_simple(icon_size * scale, icon_size * scale, Gdk::INTERP_BILINEAR);
         }
     } catch (const Glib::Error &e) {
         g_warning("Error occured while trying to load app icon: %s", e.what().c_str());
