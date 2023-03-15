bf67f7afe2194b9dc474e66dc965aa94a90a9600
wangxiaoqing <wangxiaoqing@kylinsec.com.cn>
2023-03-15 09:45:29 +0800

    feature(menu):Add pinyin search method for application
    - 添加应用程序的拼音搜索方式，先将应用程序的汉字名称转为拼音， 再根据用户输入的拼音关键词进行查找
      Related #67099
    
    Signed-off-by: wangxiaoqing <wangxiaoqing@kylinsec.com.cn>

--- lib/common.cpp
+++ lib/common.cpp
@@ -13,6 +13,7 @@
  */
 
 #include "lib/common.h"
+#include "lib/pinyin.h"
 
 namespace Kiran
 {
@@ -44,6 +45,84 @@ bool write_list_quark_to_as(Glib::RefPtr<Gio::Settings> settings,
     }
     // g_print("key: %s value: %d\n", key.c_str(), new_value.size());
     return settings->set_string_array(key, new_value);
+}
+
+static wchar_t *
+convert_chars_to_wchars(const std::string &contents)
+{
+    size_t mbs_len;
+    wchar_t *wcs;
+
+    mbs_len = mbstowcs(NULL, contents.c_str(), 0);
+    if (mbs_len == (size_t)-1)
+    {
+        return NULL;
+    }
+
+    wcs = (wchar_t *)calloc(mbs_len + 1, sizeof(wchar_t));
+    if (wcs == NULL)
+    {
+        return NULL;
+    }
+
+    if (mbstowcs(wcs, contents.c_str(), mbs_len + 1) == (size_t)-1)
+    {
+        free(wcs);
+        return NULL;
+    }
+
+    return wcs;
+}
+
+static void
+clear_space(char *pinyin)
+{
+    for (; *pinyin != '\0'; pinyin++)
+    {
+        if (*pinyin == ' ')
+        {
+            *pinyin = '\0';
+        }
+    }
+}
+
+std::list<std::string> convert_chinese_characters_to_pinyin(const std::string &contents)
+{
+    std::list<std::string> pinyin_list;
+    wchar_t *wcs;
+    int unicode;
+
+    wcs = convert_chars_to_wchars(contents);
+
+    if (wcs == NULL)
+    {
+        return pinyin_list;
+    }
+
+    for (auto wp = wcs; *wp != '\0'; wp++)
+    {
+        int unicode = *wp;
+
+        if (unicode >= CHINESE_UNICODE_START &&
+            unicode <= CHINESE_UNICODE_END)
+        {
+            char pinyin[CHINESE_UNICODE_MAX_LENGTH + 1] = {'\0'};
+            int offset = 0;
+
+            offset = (unicode - CHINESE_UNICODE_START) * CHINESE_UNICODE_MAX_LENGTH;
+
+            if (offset + CHINESE_UNICODE_MAX_LENGTH <= sizeof(chinese_basic_dict))
+            {
+                memcpy(pinyin, chinese_basic_dict + offset, CHINESE_UNICODE_MAX_LENGTH);
+                clear_space(pinyin);
+                pinyin_list.push_back(pinyin);
+            }
+        }
+    }
+
+    free(wcs);
+
+    return pinyin_list;
 }
 
 }  // namespace Kiran
